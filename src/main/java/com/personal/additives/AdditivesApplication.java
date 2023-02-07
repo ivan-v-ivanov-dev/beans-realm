30deb835df15a855166d2832b6d5846102673602
Ivan Ivanov <ivanov.biol@gmail.com>
2023-02-07 18:18:28 +0200

    Create project

---
+++ src/main/java/com/personal/additives/AdditivesApplication.java
@@ -0,0 +1,13 @@
+package com.personal.additives;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdditivesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdditivesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personal/additives/AdditivesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.additives;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdditivesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
