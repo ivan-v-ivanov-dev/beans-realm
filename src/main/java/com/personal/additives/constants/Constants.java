29a254d0d62e388c590f780629c7d1c551ee7115
Ivan Ivanov <ivanov.biol@gmail.com>
2023-02-18 10:30:29 +0200

    Add Constants class to store String constants

---
+++ src/main/java/com/personal/additives/constants/Constants.java
@@ -0,0 +1,12 @@
+package com.personal.additives.constants;
+
+public class Constants {
+
+    private Constants(){}
+
+    // Configuration constants
+    public static final String POSTGRESQL_CONNECTION_POOL= "PostgreSQL Connection Pool";
+
+    // Repository constants
+    public static final String NAME = "name";
+}
