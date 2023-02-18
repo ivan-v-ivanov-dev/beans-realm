5b841163a6a569b99be99e5c45d837a78773c28e
Ivan Ivanov <ivanov.biol@gmail.com>
2023-02-18 10:24:42 +0200

    Additive entity created.

--- src/main/java/com/personal/additives/models/Additive.java
+++ src/main/java/com/personal/additives/models/Additive.java
@@ -1,7 +1,51 @@
 package com.personal.additives.models;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "additives")
+@NoArgsConstructor
+@Getter
+@Setter
 public class Additive {
 
+    @Id
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "image")
+    private byte[] image;
+
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE}, fetch = FetchType.EAGER)
+    @JoinTable(name = "additives_versions",
+            joinColumns = @JoinColumn(name = "additive_id"),
+            inverseJoinColumns = @JoinColumn(name = "version_id"))
+    private Set<Version> version;
+
+    @ManyToOne
+    @JoinColumn(name = "creator_id")
+    private User creator;
+
+    @OneToOne
+    @JoinColumn(name = "device_id")
+    private Device device;
+
+    @OneToOne
+    @JoinColumn(name = "type_id")
+    private Type type;
+
+    @OneToOne
+    @JoinColumn(name = "tag_id")
+    private Tag tag;
+
+    @Column(name = "description")
+    private String description;
 }
