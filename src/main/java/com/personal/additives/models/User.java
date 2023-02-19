d6450d7213d5dc054334e7d077274fb1d02db672
Ivan Ivanov <ivanov.biol@gmail.com>
2023-02-19 18:54:41 +0200

    User model created

---
+++ src/main/java/com/personal/additives/models/User.java
@@ -0,0 +1,34 @@
+package com.personal.additives.models;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "image")
+    private byte[] image;
+
+    @Column(name = "enabled")
+    private boolean enabled;
+
+    @Column(name = "admin")
+    private boolean admin;
+}
