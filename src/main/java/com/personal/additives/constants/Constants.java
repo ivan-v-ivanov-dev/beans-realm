a83e7ee5b225a56f5b0a4655f5d1a9ac1c3e7615
Ivan Ivanov <ivanov.biol@gmail.com>
2023-02-20 18:21:16 +0200

    Add String constants used in @Param in Repository

--- src/main/java/com/personal/additives/constants/Constants.java
+++ src/main/java/com/personal/additives/constants/Constants.java
@@ -2,11 +2,19 @@ package com.personal.additives.constants;
 
 public class Constants {
 
-    private Constants(){}
+    private Constants() {
+    }
 
     // Configuration constants
-    public static final String POSTGRESQL_CONNECTION_POOL= "PostgreSQL Connection Pool";
+    public static final String POSTGRESQL_CONNECTION_POOL = "PostgreSQL Connection Pool";
 
     // Repository constants
     public static final String NAME = "name";
+    public static final String ADDITIVE = "additive";
+    public static final String CREATOR = "creator";
+    public static final String TYPE = "type";
+    public static final String TAG = "tag";
+    public static final String DEVICE = "device";
+    public static final String OFFSET = "offset";
+    public static final String STATUS = "status";
 }
