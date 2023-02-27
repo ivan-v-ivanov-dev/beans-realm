239649f0aa6367b5bee09efca6c79dc75d94adae
Ivan Ivanov <ivanov.biol@gmail.com>
2023-02-27 14:44:47 +0200

    RedisCacheService implemented:  - generic retrieve method created  - saveBeans method implemented

---
+++ src/main/java/com/personal/beans/service/RedisCacheServiceImpl.java
@@ -0,0 +1,40 @@
+package com.personal.beans.service;
+
+import com.personal.beans.models.Bean;
+import com.personal.beans.service.contracts.RedisCacheService;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class RedisCacheServiceImpl implements RedisCacheService {
+
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    public RedisCacheServiceImpl(RedisTemplate<String, Object> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    @Override
+    public boolean containsKey(String key) {
+        return Boolean.TRUE.equals(this.redisTemplate.opsForHash().getOperations().hasKey(key));
+    }
+
+    @Override
+    public <T> List<T> retrieve(String key, Class<T> tClazz) {
+        return this.redisTemplate
+                .opsForHash()
+                .entries(key)
+                .values()
+                .stream()
+                .map(tClazz::cast)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public void saveBeans(String key, List<Bean> beans) {
+        beans.forEach(currentBean -> this.redisTemplate.opsForHash().put(key, currentBean.getName(), currentBean));
+    }
+}
---
+++ src/main/java/com/personal/beans/service/contracts/RedisCacheService.java
@@ -0,0 +1,14 @@
+package com.personal.beans.service.contracts;
+
+import com.personal.beans.models.Bean;
+
+import java.util.List;
+
+public interface RedisCacheService {
+
+    boolean containsKey(String key);
+
+    <T> List<T> retrieve(String key, Class<T> tClazz);
+
+    void saveBeans(String key, List<Bean> beans);
+}
