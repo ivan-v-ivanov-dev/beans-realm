f2d2f9eb63fc666fd245f75c771c9e8a941bdf9c
Ivan Ivanov <ivanov.biol@gmail.com>
2023-02-27 16:18:59 +0200

    @EnableScheduling in BeansApplication

--- src/main/java/com/personal/beans/BeansApplication.java
+++ src/main/java/com/personal/beans/BeansApplication.java
@@ -6,12 +6,14 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(exclude = {
         DataSourceAutoConfiguration.class,
         DataSourceTransactionManagerAutoConfiguration.class,
         HibernateJpaAutoConfiguration.class})
 @EnableCaching
+@EnableScheduling
 public class BeansApplication {
 
     public static void main(String[] args) {
