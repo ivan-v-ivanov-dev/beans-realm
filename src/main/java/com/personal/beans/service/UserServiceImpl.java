3b12f68e3cd7090f10fa41cf36040eb4c18f5af5
Ivan Ivanov <ivanov.biol@gmail.com>
2023-02-27 15:38:46 +0200

    Registered user count cached in Redis after Postgres retrieval

--- src/main/java/com/personal/beans/service/UserServiceImpl.java
+++ src/main/java/com/personal/beans/service/UserServiceImpl.java
@@ -2,23 +2,39 @@ package com.personal.beans.service;
 
 import com.personal.beans.models.User;
 import com.personal.beans.repository.postgres.UserRepository;
+import com.personal.beans.service.contracts.RedisCacheService;
 import com.personal.beans.service.contracts.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+import static com.personal.beans.constants.Constants.*;
+
 @Service
+@Slf4j
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+    private final RedisCacheService redisCacheService;
 
-    public UserServiceImpl(UserRepository userRepository) {
+    public UserServiceImpl(UserRepository userRepository,
+                           RedisCacheService redisCacheService) {
         this.userRepository = userRepository;
+        this.redisCacheService = redisCacheService;
     }
 
     @Override
     public int userCount() {
-        return this.userRepository.userCount();
+        if (this.redisCacheService.containsKey(TOTAL_REGISTERED_USERS)) {
+            log.info(RETRIEVE_TOTAL_REGISTERED_USERS_FROM_REDIS_CACHE);
+            return this.redisCacheService.retrieve(TOTAL_REGISTERED_USERS, Integer.class).get(0);
+        }
+
+        int totalRegisteredUsers = this.userRepository.userCount();
+        this.redisCacheService.saveEntity(TOTAL_REGISTERED_USERS, totalRegisteredUsers);
+        log.info(RETRIEVE_TOTAL_REGISTERED_USERS_FROM_POSTGRES_DB);
+        return totalRegisteredUsers;
     }
 
     @Override
