360e952e189cf88cbd4fcb3a274bc42c7d91523e
Ivan Ivanov <ivanov.biol@gmail.com>
2023-03-02 21:16:19 +0200

    Add String constants:  - Add service layer constants  - Add Error message constant used in BeanDto field validation

--- src/main/java/com/personal/beans/constants/Constants.java
+++ src/main/java/com/personal/beans/constants/Constants.java
@@ -50,13 +50,30 @@ public class Constants {
     public static final String ALL_TYPES = "ALL_TYPES";
     public static final String ALL_DEVICES = "ALL_DEVICES";
 
-
     // Service constants
     public static final String EMPTY_SPACE = " ";
     public static final String UNDERSCORE = "_";
     public static final String TIME_FORMAT_TEMPLATE = "dd-MM-yyyy HH:mm:ss";
     public static final String BEAN_ = "BEAN_";
     public static final String _VERSIONS = "_VERSIONS";
+    public static final String WAITING_APPROVAL = "Waiting approval";
+    public static final String FIRST_VERSION = "First Version";
+    public static final String SHA_256 = "SHA-256";
+    public static final String ONE_AS_STRING = "1";
+    public static final String DATA_IMAGE_PNG_BASE64 = "data:image/png;base64,";
+
+    // Error messages
+    public static final String NAME_CAN_NOT_BE_EMPTY = "Name can not be empty!";
+    public static final String NAME_MUST_BE_BETWEEN_4_AND_30_SYMBOLS = "Name must be between 4 and 30 symbols!";
+    public static final String YOU_MUST_SELECT_A_TYPE = "You must select a type!";
+    public static final String YOU_MUST_SELECT_A_TAG = "You must select a tag!";
+    public static final String YOU_MUST_SELECT_A_DEVICE = "You must select a device!";
+    public static final String DESCRIPTION_CAN_NOT_BE_EMPTY = "Description can not be empty!";
+    public static final String DESCRIPTION_MUST_BE_AT_LEAST_10_SYMBOLS = "Description must be at least 10 symbols!";
+    public static final String REPOSITORY_URL_CAN_NOT_BE_EMPTY = "Repository URL can not be empty!";
+    public static final String REPOSITORY_URL_REG_EX_PATTERN = "https://github\\.com/.+/.+";
+    public static final String REPOSITORY_URL_MUST_BE_A_GIT_HUB_REPOSITORY = "Repository URL must be a GitHub repository!";
+
 
     private Constants() {
     }
