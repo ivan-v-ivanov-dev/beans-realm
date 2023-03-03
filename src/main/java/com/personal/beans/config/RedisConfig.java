3199f54b9e06207dc7abc754c445284dac930366
Ivan Ivanov <ivanov.biol@gmail.com>
2023-03-03 16:35:27 +0200

    (Refactor) RedisConfig:  - Implement ObjectMapper to parse JSON LocalDate to POJO LocalDate  - Implement Jackson2JsonRedisSerializer which uses the objectMapper  - RedisTemplate uses the Jackson2JsonRedisSerializer  - Update nested documentation

--- src/main/java/com/personal/beans/config/RedisConfig.java
+++ src/main/java/com/personal/beans/config/RedisConfig.java
@@ -1,12 +1,17 @@
 package com.personal.beans.config;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 
 /**
  * RedisStandaloneConfiguration official documentation:
@@ -15,7 +20,8 @@ import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSeriali
  * <p>
  * Must use JedisConnectionFactor, because the LettuceConnectionFactory setPassword(), setHost(), setPort()
  * methods are deprecated.
- * Must use GenericJackson2JsonRedisSerializer.
+ * Must use Jackson2JsonRedisSerializer with ObjectMapper to parse LocalDate fields from
+ * JSON (in Redis) to POJO (in Java).
  */
 @Configuration
 public class RedisConfig {
@@ -38,10 +44,27 @@ public class RedisConfig {
     }
 
     @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        objectMapper.activateDefaultTyping(LaissezFaireSubTypeValidator.instance, ObjectMapper.DefaultTyping.NON_FINAL);
+        return objectMapper;
+    }
+
+    @Bean
+    public Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer() {
+        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer =
+                new Jackson2JsonRedisSerializer<>(Object.class);
+        jackson2JsonRedisSerializer.setObjectMapper(objectMapper());
+        return jackson2JsonRedisSerializer;
+    }
+
+    @Bean
     public RedisTemplate<String, Object> redisTemplate() {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(jedisConnectionFactory());
-        template.setDefaultSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setDefaultSerializer(jackson2JsonRedisSerializer());
         template.setEnableTransactionSupport(true);
         return template;
     }
