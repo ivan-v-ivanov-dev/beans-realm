cd5d90dbbb6c03c4bdf9402b5edded25aa124a8e
Ivan Ivanov <ivanov.biol@gmail.com>
2023-03-16 18:53:40 +0200

    Add Transient field uploadedBeans in User entity

--- src/main/java/com/personal/beans/models/User.java
+++ src/main/java/com/personal/beans/models/User.java
@@ -5,10 +5,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.io.Serializable;
 
 @Entity
@@ -31,4 +28,7 @@ public class User implements Serializable {
 
     @Column(name = "admin")
     private boolean admin;
+
+    @Transient
+    private int uploadedBeans;
 }
