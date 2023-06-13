dd3d8aa08bb2e059930b921b886c938152f9b3bc
Ivan Ivanov <ivanov.biol@gmail.com>
2023-06-13 19:19:24 +0300

    Implement Role entity

---
+++ src/main/java/com/personal/beans/models/Role.java
@@ -0,0 +1,28 @@
+package com.personal.beans.models;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "roles")
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Role implements Serializable {
+
+    @Id
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+}
