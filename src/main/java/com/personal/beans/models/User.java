2bac125a08f0de4b7caea1d33b2381a97138154b
Ivan Ivanov <ivanov.biol@gmail.com>
2023-06-14 10:10:50 +0300

    In User entity:  - implements UserDetails interface  - add password field  - add Role field  - override methods from UserDetails interface and add JsonIgnore for them

--- src/main/java/com/personal/beans/models/User.java
+++ src/main/java/com/personal/beans/models/User.java
@@ -1,12 +1,17 @@
 package com.personal.beans.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
-import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
@@ -14,7 +19,7 @@ import java.io.Serializable;
 @Getter
 @Setter
 @ToString
-public class User implements Serializable {
+public class User implements UserDetails {
 
     @Id
     @Column(name = "id")
@@ -23,12 +28,43 @@ public class User implements Serializable {
     @Column(name = "username")
     private String username;
 
+    @Column(name = "password")
+    private String password;
+
     @Column(name = "enabled")
     private boolean enabled;
 
     @Column(name = "admin")
     private boolean admin;
 
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
+
     @Transient
     private int uploadedBeans;
+
+    @JsonIgnore
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(this.role.getName()));
+    }
+
+    @JsonIgnore
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @JsonIgnore
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @JsonIgnore
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
 }
